4c8d4352b1fa91f8d3b72781f43c9d3f9ecf235b
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2023-09-27 22:10:24 +0200

    le faltan dos cosas y listo

--- optimización_NL_con_CVXPY.py
+++ optimización_NL_con_CVXPY.py
@@ -8,17 +8,18 @@ el presupuesto total
 rentabilidades de cada canal
 """
 import cvxpy as cp
+import matplotlib.pyplot as plt
+import numpy as np
+
+from optimización_NL_sin_CVXPY import greedy_optimization
 
 def main():
-    try:
-        TOTAL_BUDGET = float(input("Enter the total budget: "))
-        TOTAL_BUDGET = round(TOTAL_BUDGET,2)
-    except ValueError:
-        print("Please enter a number")
-        exit(1)
+    TOTAL_BUDGET = 1000000
     alphas = [-9453.72,-8312.84,-7371.33]
     betas  = [8256.21,7764.20,7953.36]
 
+    # Linearly spaced numbers
+    x = np.linspace(1, TOTAL_BUDGET, TOTAL_BUDGET)
 
     # Variables
     google   = cp.Variable(pos=True)
@@ -36,6 +37,35 @@ def main():
     # Solve
     prob = cp.Problem(obj, constraint)
     prob.solve(solver='ECOS', verbose=False)
+
+
+    # Plot the functions and the results
+    fig = plt.figure(figsize=(10, 5), dpi=300)
+    plt.plot(x, alphas[0] + betas[0] * np.log(x), color='red', label='Google Ads')
+    plt.plot(x, alphas[1] + betas[1] * np.log(x), color='blue', label='Facebook Ads')
+    plt.plot(x, alphas[2] + betas[2] * np.log(x), color='green', label='Twitter Ads')
+
+    # Plot optimal points
+    plt.scatter([google.value, facebook.value, twitter.value],
+                [alphas[0] + betas[0] * np.log(google.value),
+                alphas[1] + betas[1] * np.log(facebook.value),
+                alphas[2] + betas[2] * np.log(twitter.value)],
+                marker="+", color='black', zorder=10)
+
+    plt.xlabel('Budget ($)')
+    plt.ylabel('Returns ($)')
+    plt.legend()
+    plt.show()
+    # List to store the best objective value for each number of iterations
+    best_obj_list = []
+
+    # Range of number of iterations to test
+    num_iterations_range = np.logspace(0, 6, 20).astype(int)
+
+    # Run the greedy algorithm for each number of iterations and store the best objective value
+    for num_iterations in num_iterations_range:
+        _, best_obj = greedy_optimization(TOTAL_BUDGET, alphas, betas, num_iterations)
+        best_obj_list.append(best_obj)
 
     # Print solution
     print('='*59 + '\n' + ' '*24 + 'Solution' + ' '*24 + '\n' + '='*59)
