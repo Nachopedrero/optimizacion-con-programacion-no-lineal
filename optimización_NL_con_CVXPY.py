a90e4664c792f40017821b876c2a56d7b62cd2bb
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2023-09-27 21:56:09 +0200

    Todo acabado, solo falta probarlo

--- optimización_NL_con_CVXPY.py
+++ optimización_NL_con_CVXPY.py
@@ -9,15 +9,26 @@ rentabilidades de cada canal
 """
 import cvxpy as cp
 
-# Variables
-google   = cp.Variable(pos=True)
-facebook = cp.Variable(pos=True)
-twitter  = cp.Variable(pos=True)
+def main():
+    try:
+        TOTAL_BUDGET = float(input("Enter the total budget: "))
+        TOTAL_BUDGET = round(TOTAL_BUDGET,2)
+    except ValueError:
+        print("Please enter a number")
+        exit(1)
+    alphas = [-9453.72,-8312.84,-7371.33]
+    betas  = [8256.21,7764.20,7953.36]
 
-# Constraint
-constraint = [google + facebook + twitter <= TOTAL_BUDGET]
 
-# Objective
-obj = cp.Maximize(alphas[0] + betas[0] * cp.log(google)
-                + alphas[1] + betas[1] * cp.log(facebook)
-                + alphas[2] + betas[2] * cp.log(twitter))
+    # Variables
+    google   = cp.Variable(pos=True)
+    facebook = cp.Variable(pos=True)
+    twitter  = cp.Variable(pos=True)
+
+    # Constraint
+    constraint = [google + facebook + twitter <= TOTAL_BUDGET]
+
+    # Objective
+    obj = cp.Maximize(alphas[0] + betas[0] * cp.log(google)
+                    + alphas[1] + betas[1] * cp.log(facebook)
+                    + alphas[2] + betas[2] * cp.log(twitter))
